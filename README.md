e8e3e26d2256e6a8a28c7793221444efa0379cc1
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-03-22 08:40:59 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# jmx-exporter
 
-## Description
+## A process for exposing JMX Beans via HTTP for Prometheus consumption.
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "jmx-exporter" {
+  name = "jmx-exporter"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/jmx-exporter" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "jmx-exporter" {
+  repository = aws_ecr_repository.jmx-exporter.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.jmx-exporter.repository_url
 }
